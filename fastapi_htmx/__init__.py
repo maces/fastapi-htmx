b9b47fc74d82e8028554d1cc842fcc9a851977d1
maces <maces@users.noreply.github.com>
2024-01-13 00:04:33 +0100

    Add support for multiple template collections

--- fastapi_htmx/__init__.py
+++ fastapi_htmx/__init__.py
@@ -1,5 +1,6 @@
 """Extension for FastAPI to make HTMX easier to use."""
 
 from .htmx import HXRequest as HXRequest
+from .htmx import TemplateSpec as TemplateSpec
 from .htmx import htmx as htmx
 from .htmx import htmx_init as htmx_init
--- fastapi_htmx/htmx.py
+++ fastapi_htmx/htmx.py
@@ -2,13 +2,35 @@
 import inspect
 import logging
 from collections.abc import Callable, Mapping
+from dataclasses import dataclass
 from functools import wraps
-from typing import Optional
+from typing import Dict, Optional, Union
 
 from fastapi import HTTPException, Request
 from fastapi.templating import Jinja2Templates
 
-templates_path: Optional[Jinja2Templates] = None
+TemplatePath = Union[Jinja2Templates, Dict[str, Jinja2Templates]]
+TemplateName = str
+templates_path: Optional[TemplatePath] = None
+templates_file_extension: Optional[str] = None
+
+
+@dataclass
+class TemplateFileInfo:
+    """A template collection and template name ready for use."""
+
+    __slots__ = ("collection", "file_name")
+    collection: Jinja2Templates
+    file_name: TemplateName
+
+
+@dataclass
+class TemplateSpec:
+    """A template specification in case multiple collections are used."""
+
+    __slots__ = ("collection_name", "template_name")
+    collection_name: str
+    template_name: TemplateName
 
 
 class MissingFullPageTemplateError(HTTPException):
@@ -27,29 +49,58 @@ class MissingHTMXInitError(Exception):
     pass
 
 
+class InvalidHTMXInitError(Exception):
+    """FastAPI-HTMX was not initialized properly."""
+
+    pass
+
+
 class HXRequest(Request):
     """FastAPI Request Object with HTMX additions."""
 
     hx_request: bool = False
 
 
+def _get_template_name(name: str, file_extension: str) -> TemplateFileInfo:
+    if isinstance(name, TemplateSpec):
+        try:
+            templates_collection_path = templates_path[name.collection_name]
+            template_name_in_collection = name.template_name
+        except KeyError:
+            raise InvalidHTMXInitError(
+                "FASTAPI-HTMX was not initialized properly."
+                "Please specify all collections of templates used in the decorators"
+            )
+    else:
+        templates_collection_path = templates_path
+        template_name_in_collection = name
+
+    if file_extension is None:
+        file_extension = templates_file_extension
+
+    template_file_name_in_collection = f"{template_name_in_collection}.{file_extension}"
+
+    return TemplateFileInfo(collection=templates_collection_path, file_name=template_file_name_in_collection)
+
+
 def htmx(  # noqa: C901
-    partial_template_name: str,
-    full_template_name: Optional[str] = None,
+    partial_template_name: Union[TemplateSpec, TemplateName],
+    full_template_name: Optional[Union[TemplateSpec, TemplateName]] = None,
     partial_template_constructor: Optional[Callable] = None,
     full_template_constructor: Optional[Callable] = None,
-    template_extension: str = "jinja2",
+    template_extension: Optional[str] = None,
 ) -> Callable:
     """Decorator for FastAPI routes to make HTMX easier to use.
 
     Args:
-        partial_template_name (str): A Template for the partial to use.
-        full_template_name (Optional[str], optional): The full page template name. Defaults to None.
-        partial_template_constructor (Optional[Callable], optional): A Callable returning the needed variables for the
-                                                                  template. Defaults to None.
-        full_template_constructor (Optional[Callable], optional): A Callable returning the needed variables for the
-                                                               template. Defaults to None.
-        template_extension (str, optional): The template extension to use. Defaults to "jinja2".
+        partial_template_name (Union[TemplateSpec, TemplateName]): A Template for the partial to use.
+        full_template_name (Optional[Union[TemplateSpec, TemplateName]]): The full page template name.
+                                                                                        Defaults to None.
+        partial_template_constructor (Optional[Callable]): A Callable returning the needed variables for the template.
+                                                           Defaults to None.
+        full_template_constructor (Optional[Callable]): A Callable returning the needed variables for the template.
+                                                        Defaults to None.
+        template_extension (Optional[str]): The template extension to use. Defaults to None.
 
     Raises:
         MissingFullPageTemplateError: If a full page is required bu no template is specified.
@@ -105,8 +156,9 @@ def htmx(  # noqa: C901
                     "before using the '@htmx(...)' decorator"
                 )
 
-            return templates_path.TemplateResponse(
-                f"{template_name}.{template_extension}",
+            template = _get_template_name(name=template_name, file_extension=template_extension)
+            return template.collection.TemplateResponse(
+                template.file_name,
                 {
                     "request": request,
                     **response,
@@ -122,11 +174,15 @@ def _is_fullpage_request(request: Request) -> bool:
     return "HX-Request" not in request.headers or request.headers["HX-Request"].lower() != "true"
 
 
-def htmx_init(templates: Jinja2Templates):
+def htmx_init(templates: TemplatePath, file_extension: str = "jinja2"):
     """Initialize the HTMX extension.
 
     Args:
-        templates (Jinja2Templates): The configured template instance to use.
+        templates (TemplatePath): The configured template instance to use.
+                                  Or multiple template collections distinguished by a key.
+        file_extension: (str): The file extension to use for all templates. Can be individually overriden.
     """
     global templates_path
     templates_path = templates
+    global templates_file_extension
+    templates_file_extension = file_extension
